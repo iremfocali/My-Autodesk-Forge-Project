3b59ef5c0c8be73d72b4da1fda0727e76af88289
iremfocali <irem.focali@hotmail.com>
2021-08-31 14:04:04 +0300

    My internship project
    My internship project was designed with autodesk forge technology

---
+++ ViewMyModels_iremfocali/wwwroot/js/rotateobjects.js
@@ -0,0 +1,78 @@
+﻿class RotateObjects extends Autodesk.Viewing.Extension {
+    constructor(viewer, options) {
+        super(viewer, options);
+        this._group = null;
+        this._button = null;
+        var isActivate = false;
+        var fragProxy = null;
+    }
+    load() {
+        console.log('Rotate extension has been loaded');
+        return true;
+    }
+    addEventListener() {
+        viewer.addEventListener(Autodesk.Viewing.AGGREGATE_SELECTION_CHANGED_EVENT, this.onSelected);
+       
+    }
+    onSelected(event) {
+        console.log("ROTATE EXTENSION :")
+        this.fragProxy = viewer.impl.getFragmentProxy(viewer.model, event.selections[0].fragIdsArray[0]);
+        this.fragProxy.getAnimTransform();
+        console.log(this.fragProxy)
+        this.fragProxy.position = new THREE.Vector3(0, 0, 0);
+        this.fragProxy.quaternion = new THREE.Quaternion().setFromAxisAngle(
+            new THREE.Vector3(1, 0, 0),
+            document.getElementById("degreeX").value * Math.PI / 180);
+        this.fragProxy.quaternion = new THREE.Quaternion().setFromAxisAngle(
+            new THREE.Vector3(0, 1, 0),
+            document.getElementById("degreeY").value * Math.PI / 180);
+        this.fragProxy.quaternion = new THREE.Quaternion().setFromAxisAngle(
+            new THREE.Vector3(0, 0, 1),
+            document.getElementById("degreeZ").value * Math.PI / 180);
+        this.fragProxy.updateAnimTransform();
+        viewer.impl.sceneUpdated(true);
+    }
+
+    unload() {
+        viewer.removeEventListener(Autodesk.Viewing.AGGREGATE_SELECTION_CHANGED_EVENT, this.onSelected);
+        console.log('deactivate');
+        // Clean our UI elements if we added any
+        if (this._group) {
+            this._group.removeControl(this._button);
+            if (this._group.getNumberOfControls() === 0) {
+                this.viewer.toolbar.removeControl(this._group);
+            }
+        }
+        console.log('RotateObjects has been unloaded');
+        return true;
+    }
+
+    onToolbarCreated() {
+        // Create a new toolbar group if it doesn't exist
+        this._group = this.viewer.toolbar.getControl('allRotateObjectsToolbar');
+        if (!this._group) {
+            this._group = new Autodesk.Viewing.UI.ControlGroup('allRotateObjectsToolbar');
+            this.viewer.toolbar.addControl(this._group);
+        }
+
+        // Add a new button to the toolbar group
+        this._button = new Autodesk.Viewing.UI.Button('RotateObjectsButton');
+        this._button.onClick = (ev) => {            
+            this.isActivate = !this.isActivate;
+            if (this.isActivate) {
+                console.log("add")
+                this.addEventListener();
+            } else {
+                console.log("remove")
+                viewer.removeEventListener(Autodesk.Viewing.AGGREGATE_SELECTION_CHANGED_EVENT, this.onSelected);
+            }
+
+        };
+        this._button.setToolTip('Rotate Objects Extension');
+        this._button.addClass('RotateObjectsIcon');
+        this._group.addControl(this._button);
+    }
+}
+
+Autodesk.Viewing.theExtensionManager.registerExtension('iremfocali.RotateObjects', RotateObjects);
+
---
+++ ViewMyModels_iremfocali/wwwroot/js/sizefragsextension.js
@@ -0,0 +1,77 @@
+﻿class sizefragextension extends Autodesk.Viewing.Extension {
+    constructor(viewer, options) {
+        super(viewer, options);
+        this._group = null;
+        this._button = null;
+                
+    }
+    load() {
+        console.log('Size of frag extension has been loaded');
+        return true;
+    }
+    addEventListener() {
+        viewer.addEventListener(Autodesk.Viewing.AGGREGATE_SELECTION_CHANGED_EVENT, this.onSelected);
+    }
+
+    // on selected
+    onSelected(event) {
+        this.fragProxy = viewer.impl.getFragmentProxy(viewer.model, event.selections[0].fragIdsArray[0]);
+        this.fragProxy.getAnimTransform();
+        console.log(this.fragProxy)
+
+        let frags = viewer.model.getFragmentList();
+        let bbox = new THREE.Box3();
+        let fragId = event.selections[0].fragIdsArray[0];
+        frags.getWorldBounds(fragId, bbox)
+        this.originalX = (bbox.max.x - bbox.min.x);
+        this.originalY = (bbox.max.y - bbox.min.y);
+        this.originalZ = (bbox.max.z - bbox.min.z);
+
+        document.getElementById("scaleX").value = this.originalX;
+        document.getElementById("scaleY").value = this.originalY;
+        document.getElementById("scaleZ").value = this.originalZ;
+
+    }
+
+    unload() {
+        viewer.removeEventListener(Autodesk.Viewing.AGGREGATE_SELECTION_CHANGED_EVENT, this.onSelected);
+        console.log('deactivate');
+        // Clean our UI elements if we added any
+        if (this._group) {
+            this._group.removeControl(this._button);
+            if (this._group.getNumberOfControls() === 0) {
+                this.viewer.toolbar.removeControl(this._group);
+            }
+        }
+        console.log('Triple Extension has been unloaded');
+        return true;
+    }
+
+    onToolbarCreated() {
+        // Create a new toolbar group if it doesn't exist
+        this._group = this.viewer.toolbar.getControl('allTripleExtensionsToolbar');
+        if (!this._group) {
+            this._group = new Autodesk.Viewing.UI.ControlGroup('allTripleExtensionsToolbar');
+            this.viewer.toolbar.addControl(this._group);
+    }
+
+
+    // Add a new button to the toolbar group
+    this._button = new Autodesk.Viewing.UI.Button('allTripleExtensionButton');
+    this._button.onClick = (ev) => {
+        this.isActivate = !this.isActivate;
+        if (this.isActivate) {
+            console.log("add")
+            this.addEventListener();
+
+        } else {
+            console.log("remove")
+            viewer.removeEventListener(Autodesk.Viewing.AGGREGATE_SELECTION_CHANGED_EVENT, this.onSelected);
+        }
+    };
+    this._button.setToolTip('Size of Frags Extension');
+        this._button.addClass('sizefragextensionIcon');
+    this._group.addControl(this._button);
+    }
+ }
+Autodesk.Viewing.theExtensionManager.registerExtension('iremfocali.sizefragextension', sizefragextension);
